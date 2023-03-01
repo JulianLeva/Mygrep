fa83f51a2866a0c3bce6bdab34ea5259170e8a16
JulianLeva <julian.leva@tuni.fi>
2023-03-01 14:31:12 +0200

    1 variable changed

--- Mygrep.cpp
+++ Mygrep.cpp
@@ -18,7 +18,7 @@ int main(int argc, char* argv[]) {
     if (argc == 1) {
 
         string statement, characters;
-        int position;
+        size_t position;
 
         cout << "Give a string from which to search for: ";
         getline(cin, statement);
