8336b98a3af66be0aadf47e51cd383db09854d5c
JulianLeva <julian.leva@tuni.fi>
2023-03-01 14:17:40 +0200

    Increment 4

--- Mygrep.cpp
+++ Mygrep.cpp
@@ -2,14 +2,19 @@
 #include <iostream>
 #include<string>
 #include<fstream>
+#include<algorithm>
 
 using namespace std;
 
-void options_line_numbering_occurances(string, ifstream &);
+void options_line_numbering_occurances(string, ifstream&);
 void options_line_numbering(string, ifstream&);
 void options_occurances(string, ifstream&);
+void options_reverse(string, ifstream&);
+void options_ignore_case(string, ifstream&);
+void options_everything(string, ifstream&);
+bool finding_occurances(string, string);
 
-int main(int argc, char* argv[]) { 
+int main(int argc, char* argv[]) {
     if (argc == 1) {
 
         string statement, characters;
@@ -21,7 +26,8 @@ int main(int argc, char* argv[]) {
         cout << "Give search string: ";
         getline(cin, characters);
 
-        position = statement.find(characters);
+        position = statement.find(characters); // Finds if string characters is included in string statement which results to the starting position of the found string.
+        // Compares the found position to string null position if they are not the same string statement includes string characters.
         if (position != string::npos) {
             cout << "\"" << characters << "\" found in \"" << statement << "\" in position " << position << endl;
         }
@@ -39,19 +45,24 @@ int main(int argc, char* argv[]) {
         inputFile.open(argv[2]);
 
         if (inputFile) {
+            // Does this if open was succesfull.
+
             while (getline(inputFile, line)) {
+                //Goes through the file line by line.
                 if (line.find(wordtofind) != string::npos) {
                     cout << line << endl;
                     found = true;
                 }
             }
-            
+
+            // If nothing was found prints this.
             if (found == false) {
                 cout << "\"" << wordtofind << "\" can't be found in the file\n";
             }
 
             inputFile.close();
         }
+        // If file couldn't be opened.
         else {
             cout << "Error opening the file.\n";
         }
@@ -61,32 +72,51 @@ int main(int argc, char* argv[]) {
         string wordtofind = argv[2], option = argv[1];
         ifstream inputFile;
 
-        inputFile.open(argv[3]);
-
-        if (inputFile) {
-
-            if (option == "-ol") {
-                options_line_numbering(wordtofind, inputFile);
+        // Ensures that nothing goes wrong opening the file.
+        try {
+            inputFile.open(argv[3]);
+            // If file can't be opened.
+            if (!inputFile) {
+                throw - 1;
             }
-
-            else if (option == "-oo") {
-                options_occurances(wordtofind, inputFile);
-            }
-
-            else if (option == "-olo") {
-                options_line_numbering_occurances(wordtofind, inputFile);
-            }
-
-            inputFile.close();
         }
-        else {
-            cout << "Error opening the file.\n";
+        catch (int e) {
+            // Exeption for file not opening correctly.
+            cout << "An exception occurred. Exception Nr. " << e << endl;
+            cout << "File " << "\"" << argv[3] << "\"" << " could not be found\n";
+            return 1;
         }
+
+        if (option == "-ol") {
+            options_line_numbering(wordtofind, inputFile);
+        }
+
+        else if (option == "-oo") {
+            options_occurances(wordtofind, inputFile);
+        }
+
+        else if (option == "-olo") {
+            options_line_numbering_occurances(wordtofind, inputFile);
+        }
+
+        else if (option == "-or") {
+            options_reverse(wordtofind, inputFile);
+        }
+
+        else if (option == "-oi") {
+            options_ignore_case(wordtofind, inputFile);
+        }
+
+        else if (option == "-olori") {
+            options_everything(wordtofind, inputFile);
+        }
+
+        inputFile.close();
     }
-    
 
+    // If user gives wrong number of arguments in the cmd line.
     else {
-        cout << "Wrong cmd line arguments given.\n";
+        cout << "Wrong number of cmd line arguments given.\n";
     }
 
     return 0;
@@ -98,12 +128,12 @@ void options_line_numbering_occurances(string word, ifstream& file) {
     bool found = false;
 
     while (getline(file, line)) {
-        line_number++;
+        line_number++; // Counts the line numbers.
 
         if (line.find(word) != string::npos) {
             cout << line_number << ":\t" << line << endl;
             found = true;
-            counter++;
+            counter++; // Counts the amount of lines found containing the given string.
         }
     }
 
@@ -151,4 +181,76 @@ void options_occurances(string word, ifstream& file) {
     if (found == false) {
         cout << "\"" << word << "\" can't be found in the file\n";
     }
+}
+
+void options_reverse(string word, ifstream& file) {
+    string line;
+    bool found;
+
+    while (getline(file, line)) {
+        found = false;
+        if (line.find(word) != string::npos) {
+            found = true;
+        }
+
+        // Prints everything unless the string is found.
+        if (found == false) {
+            cout << line << endl;
+        }
+    }
+}
+
+void options_ignore_case(string word, ifstream& file) {
+    string line;
+    bool notfound = true;
+
+    while (getline(file, line)) {
+        if (finding_occurances(word, line) == true) {
+            cout << line << endl;
+            notfound = false;
+        }
+    }
+
+    if (notfound == true) {
+        cout << "\"" << word << "\" can't be found in the file\n";
+    }
+}
+
+bool finding_occurances(string word, string fileline) {
+    bool found;
+    // Transforms parameter strings to lowercase.
+    transform(word.begin(), word.end(), word.begin(), ::tolower);
+    transform(fileline.begin(), fileline.end(), fileline.begin(), ::tolower);
+
+    if (fileline.find(word) != string::npos) {
+        found = true;
+    }
+    else {
+        found = false;
+    }
+
+    return found;
+}
+
+void options_everything(string word, ifstream& file) {
+    int counter = 0, line_number = 0;
+    string line;
+    bool found;
+
+    while (getline(file, line)) {
+        found = false;
+        line_number++;
+
+        if (finding_occurances(word, line) == true) {
+            found = true;
+            counter++;
+        }
+
+        if (found == false) {
+            cout << line_number << ":" << line << endl;
+        }
+    }
+
+    cout << "\nOccurrences of lines NOT containing \"" << word << "\": "
+        << line_number - counter << endl;
 }
